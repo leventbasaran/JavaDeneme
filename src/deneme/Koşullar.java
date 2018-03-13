fc50b903912ffa52858eedea771aee32aa232561
Levent Başaran <leventbasarann@gmail.com>
2018-03-13 14:45:41 +0300

    İlk gonderme

---
+++ src/deneme/Koşullar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deneme;
+
+/**
+ *
+ * @author Levent Başaran
+ */
+public class Koşullar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int i = 8;
+    if(i<10){
+    System.out.println("i'nin değeri 10dur");
+}
+    else {
+        System.out.println("İ'nin değeri 10dan farklıdır. ");
+    }
+    System.out.println("koşulsuz");
+    }
+    
+}
